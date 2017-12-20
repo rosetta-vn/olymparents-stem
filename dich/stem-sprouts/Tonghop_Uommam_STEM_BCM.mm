4d5ed7b71e8adcfc782dead2dff05d2cef8ef776
rosettavn <doanminhdang@gmail.com>
2017-12-20 17:03:13 +0100

    Thêm bản sơ đồ tổng hợp Freemind tóm tắt các tài liệu dịch trong bộ STEM Sprouts

---
+++ dich/stem-sprouts/Tonghop_Uommam_STEM_BCM.mm
@@ -0,0 +1,172 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ffcc66" COLOR="#0033ff" CREATED="1513602383434" ID="ID_830339434" MODIFIED="1513785360835" STYLE="fork" TEXT="B&#x1ed9; t&#xe0;i li&#x1ec7;u &quot;&#x1af;&#x1a1;m m&#x1ea7;m STEM&quot;&#xa;c&#x1ee7;a Boston Children&apos;s Museum">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#00b439" CREATED="1513602462873" ID="ID_836189883" MODIFIED="1513607364648" POSITION="right" STYLE="fork" TEXT="Ho&#x1ea1;t &#x111;&#x1ed9;ng STEM&#xa;(STEM Activities)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#0000ff" CREATED="1513607375372" ID="ID_1678022826" MODIFIED="1513607520447" TEXT="Khoa h&#x1ecd;c">
+<edge COLOR="#0099ff"/>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#0033ff" CREATED="1513602923240" ID="ID_1384186639" LINK="https://www.dropbox.com/s/xian53qi3kle6u8/STEM.Activity_sheet.Science_Bubbles_dich.pdf?dl=0" MODIFIED="1513764154169" STYLE="fork" TEXT="Bong b&#xf3;ng (Science_Bubbles)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#0033ff" CREATED="1513602923240" ID="ID_1308333347" LINK="https://www.dropbox.com/s/iuzmb7g0rihw56l/STEM.Activity_sheet.Science_ShadowPlay_dich_v3.pdf?dl=0" MODIFIED="1513764194403" STYLE="fork" TEXT="Ch&#x1a1;i v&#x1edb;i c&#xe1;i b&#xf3;ng (Science_ShadowPlay)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#0033ff" CREATED="1513602923241" ID="ID_1466568272" LINK="https://www.dropbox.com/s/3dkojiflt6qknc1/STEM.Activity_sheet.Science_AirWind_dich.pdf?dl=0" MODIFIED="1513764222592" STYLE="fork" TEXT="Kh&#xf4;ng kh&#xed; v&#xe0; gi&#xf3; (Science_AirWind)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#ff3333" CREATED="1513607378101" ID="ID_316802655" MODIFIED="1513607537463" TEXT="C&#xf4;ng ngh&#x1ec7;">
+<edge COLOR="#0099ff"/>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff3333" CREATED="1513602923240" ID="ID_1257614997" MODIFIED="1513606900528" STYLE="fork" TEXT="L&#x1ef1;c h&#xfa;t b&#xed; &#x1ea9;n (Technology_Magnets)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#ff3333" CREATED="1513602923241" ID="ID_422647538" LINK="https://www.dropbox.com/s/1w4jnyz6d8d64kt/STEM.Activity_sheet_Technology_MagnifyingMagic_dich_v2.pdf?dl=0" MODIFIED="1513764212849" STYLE="fork" TEXT="K&#xed;nh l&#xfa;p k&#x1ef3; di&#x1ec7;u (Technology_MagnifyingMagic)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#ff3333" CREATED="1513602923241" ID="ID_392921614" LINK="https://www.dropbox.com/s/1fvkuqe3fkg2q95/STEM.Activity_sheet_Technology_ShapeUp_dich.pdf?dl=0" MODIFIED="1513764231599" STYLE="fork" TEXT="T&#x1ea1;o h&#xec;nh n&#xe0;o (Technology_ShapeUp)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#009900" CREATED="1513607382005" ID="ID_61252318" MODIFIED="1513607557460" TEXT="K&#x1ef9; thu&#x1ead;t">
+<edge COLOR="#0099ff"/>
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#009933" CREATED="1513602923239" ID="ID_1912303810" LINK="https://www.dropbox.com/s/lyagun7c86810k0/STEM.Activity_sheet.Engineering_PaperBridge_dich.pdf?dl=0" MODIFIED="1513764110395" STYLE="fork" TEXT="C&#xf9;ng x&#xe2;y c&#x1ea7;u gi&#x1ea5;y (Engineering_PaperBridge)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#009933" CREATED="1513602923239" ID="ID_456177219" LINK="https://www.dropbox.com/s/0weie4dp6vs9jxq/STEM.Activity_sheet.Engineering_Towers_dich.pdf?dl=0" MODIFIED="1513764123097" STYLE="fork" TEXT="C&#xf9;ng x&#xe2;y th&#xe1;p cao (Engineering_Towers)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#009933" CREATED="1513602923240" ID="ID_534499858" LINK="https://www.dropbox.com/s/1ouhudb2x76024i/STEM.Activity_sheet.Engineering_RecycleArt_dich.pdf?dl=0" MODIFIED="1513764134262" STYLE="fork" TEXT="Ngh&#x1ec7; thu&#x1ead;t t&#x1eeb; &#x111;&#x1ed3; t&#xe1;i ch&#x1ebf; (Engineering_RecycleArt)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#ff9933" CREATED="1513607367725" ID="ID_1168139295" MODIFIED="1513607612657" TEXT="To&#xe1;n">
+<edge COLOR="#0099ff"/>
+<cloud COLOR="#f0f0f0"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff9900" CREATED="1513602923238" ID="ID_1224571749" LINK="https://www.dropbox.com/s/2muile8j605wi9i/STEM.Activity_sheet.Math_ColorShape_dich.pdf?dl=0" MODIFIED="1513764097448" STYLE="fork" TEXT="Th&#xe1;m t&#x1eed; m&#xe0;u s&#x1eaf;c v&#xe0; h&#xec;nh kh&#x1ed1;i (Math_ColorShape)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#ff9900" CREATED="1513602923240" ID="ID_395996310" LINK="https://www.dropbox.com/s/4nismscjzg8xcpd/STEM.Activity_sheet.Math_Fishbowl_dich.pdf?dl=0" MODIFIED="1513764168096" STYLE="fork" TEXT="B&#x1ec3; c&#xe1; To&#xe1;n h&#x1ecd;c (Math_Fishbowl)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#ff9900" CREATED="1513602923240" ID="ID_743217984" LINK="https://www.dropbox.com/s/w23qryetjomw4ml/STEM.Activity_sheet.Math_TB_Picnic_dich_v2.pdf?dl=0" MODIFIED="1513764183102" STYLE="fork" TEXT="Chuy&#x1ebf;n d&#xe3; ngo&#x1ea1;i c&#x1ee7;a g&#x1ea5;u Teddy (Math_TB_Picnic)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1513602474405" ID="ID_640348045" MODIFIED="1513606900528" POSITION="right" STYLE="fork" TEXT="G&#x1ee3;i &#xfd; b&#x1ecf; t&#xfa;i cho cha m&#x1eb9;&#xa;(Parent Tips)">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1513602943555" ID="ID_962358003" LINK="https://www.dropbox.com/s/j6ks4bq9q7y54lr/STEM.Tip.Sheet.Science_dich_v2.pdf?dl=0" MODIFIED="1513764304059" STYLE="fork" TEXT="Khoa h&#x1ecd;c (Science)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513602943555" ID="ID_1506183924" LINK="https://www.dropbox.com/s/pvdvbwimz8w40hw/STEM.Tip.Sheet.Technology_dich_v2.pdf?dl=0" MODIFIED="1513764341048" STYLE="fork" TEXT="C&#xf4;ng ngh&#x1ec7; (Technology)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513602943553" ID="ID_1261938933" LINK="https://www.dropbox.com/s/hqla9h7ji61rufr/STEM.Tip.Sheet.Engineering_dich_v2.pdf?dl=0" MODIFIED="1513764261628" STYLE="fork" TEXT="K&#x1ef9; thu&#x1ead;t (Engineering)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513602943554" ID="ID_671349047" LINK="https://www.dropbox.com/s/f44hbnwonpw2fvl/STEM.Tip.Sheet.Math_dich_v1.pdf?dl=0" MODIFIED="1513764289798" STYLE="fork" TEXT="To&#xe1;n (Math)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#0099ff" CREATED="1513602943555" ID="ID_1463780539" MODIFIED="1513607600682" STYLE="fork" TEXT="Nh&#xec;n (Seeing)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#0099ff" CREATED="1513602943554" ID="ID_448705744" LINK="https://www.dropbox.com/s/y0ud7arziiz1epa/STEM.Tip.Sheet.Hearing_dich_v3.pdf?dl=0" MODIFIED="1513764275795" STYLE="fork" TEXT="L&#x1eaf;ng nghe (Hearing)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#0099ff" CREATED="1513602943556" ID="ID_1605554939" LINK="https://www.dropbox.com/s/w632xgks989soip/STEM.Tip.Sheet.Touching_dich_v1.pdf?dl=0" MODIFIED="1513764355055" STYLE="fork" TEXT="S&#x1edd; ch&#x1ea1;m (Touching)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#0099ff" CREATED="1513602943555" ID="ID_1790298689" LINK="https://www.dropbox.com/s/ndgtenwzneo569q/STEM.Tip.Sheet.Smelling.tasting_dich_v1.pdf?dl=0" MODIFIED="1513764327949" STYLE="fork" TEXT="Ng&#x1eed;i v&#xe0; n&#x1ebf;m (Smelling_and_tasting)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1513604728659" ID="ID_1968912736" MODIFIED="1513606900528" POSITION="left" STYLE="fork" TEXT="T&#xe0;i li&#x1ec7;u b&#x1ed5; tr&#x1ee3;">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1513604747676" ID="ID_590954571" LINK="https://www.dropbox.com/s/2jou54zggz2ocgy/Science-Process-Skills-Worksheet_dich_v1.pdf?dl=0" MODIFIED="1513785423383" STYLE="fork" TEXT="B&#x1ea3;ng k&#x1ef9; n&#x103;ng th&#x1ef1;c h&#xe0;nh v&#xe0; l&#x1ead;p k&#x1ebf; ho&#x1ea1;ch&#xa;(Science-Process-Skills)">
+<edge COLOR="#990000" WIDTH="1"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_590954571" ENDARROW="Default" ENDINCLINATION="317;0;" ID="Arrow_ID_1538530028" SOURCE="ID_751978988" STARTARROW="None" STARTINCLINATION="317;0;"/>
+</node>
+<node COLOR="#990000" CREATED="1513604740147" ID="ID_12430508" LINK="https://www.dropbox.com/s/jfy20obkl05ighj/Preview_I_am_a_Scientist_v2.pdf?dl=0" MODIFIED="1513764433238" STYLE="fork" TEXT="S&#x1ed5; ghi th&#xe0;nh qu&#x1ea3; &#xa;(I am a Scientist Workbook)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513604925198" ID="ID_1160615230" LINK="https://www.dropbox.com/s/aaw48g6azhyai5s/STEMGuide_dich_v1.pdf?dl=0" MODIFIED="1513764058656" STYLE="fork" TEXT="H&#x1b0;&#x1edb;ng d&#x1eab;n gi&#x1ea3;ng d&#x1ea1;y STEM cho tr&#x1ebb; m&#x1ea7;m non&#xa;(STEM Teaching guide)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513605016878" ID="ID_1774063414" MODIFIED="1513764418817" STYLE="fork" TEXT="S&#x1ed5; ho&#x1ea1;t &#x111;&#x1ed9;ng STEM cho gia &#x111;&#xec;nh&#xa;(Family Activities Workbook)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#669900" CREATED="1513605125678" ID="ID_751978988" MODIFIED="1513785457038" POSITION="left" STYLE="fork" TEXT="Nh&#x1eef;ng k&#x1ef9; n&#x103;ng tr&#x1ebb; s&#x1ebd; h&#x1ecd;c &#x111;&#x1b0;&#x1ee3;c">
+<edge COLOR="#00b439" WIDTH="2"/>
+<arrowlink DESTINATION="ID_590954571" ENDARROW="Default" ENDINCLINATION="317;0;" ID="Arrow_ID_1538530028" STARTARROW="None" STARTINCLINATION="317;0;"/>
+<node CREATED="1513606213889" ID="ID_274215038" MODIFIED="1513785457039" STYLE="fork" TEXT="Quan s&#xe1;t">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213889" ID="ID_798003496" MODIFIED="1513785457039" STYLE="fork" TEXT="Th&#xed; nghi&#x1ec7;m">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213889" ID="ID_1524315748" MODIFIED="1513785457039" STYLE="fork" TEXT="Ghi ch&#xe9;p">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213890" ID="ID_1246826303" MODIFIED="1513785457039" STYLE="fork" TEXT="S&#x1eed; d&#x1ee5;ng c&#xf4;ng c&#x1ee5;">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213890" ID="ID_1025842879" MODIFIED="1513785457039" STYLE="fork" TEXT="Mi&#xea;u t&#x1ea3;">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213890" ID="ID_276530158" MODIFIED="1513785457039" STYLE="fork" TEXT="D&#x1ef1; &#x111;o&#xe1;n">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213890" ID="ID_1222661267" MODIFIED="1513785457039" STYLE="fork" TEXT="Gi&#x1ea3;i quy&#x1ebf;t v&#x1ea5;n &#x111;&#x1ec1;">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213890" ID="ID_495147190" MODIFIED="1513785457039" STYLE="fork" TEXT="Ph&#xe2;n lo&#x1ea1;i">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213890" ID="ID_758233567" MODIFIED="1513785457039" STYLE="fork" TEXT="T&#x1ed5;ng qu&#xe1;t h&#xf3;a">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213890" ID="ID_1650665897" MODIFIED="1513785457040" STYLE="fork" TEXT="H&#x1ee3;p t&#xe1;c">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213891" ID="ID_1162041982" MODIFIED="1513785457040" STYLE="fork" TEXT="Giao ti&#x1ebf;p">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+<node CREATED="1513606213891" ID="ID_1120147467" MODIFIED="1513785457040" STYLE="fork" TEXT="Li&#xea;n h&#x1ec7; v&#x1edb;i tr&#x1ea3;i nghi&#x1ec7;m &#x111;&#xe3; c&#xf3;">
+<edge COLOR="#0000ff" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#ff3333" CREATED="1513606448574" ID="ID_1312650155" MODIFIED="1513607146314" POSITION="left" STYLE="fork" TEXT="C&#xe1;c &quot;b&#xed; quy&#x1ebf;t&quot; d&#xe0;nh cho cha m&#x1eb9;">
+<edge COLOR="#00cc33" WIDTH="2"/>
+<node CREATED="1513606485511" ID="ID_1549390751" MODIFIED="1513607338160" STYLE="fork" TEXT="&#x1ede; b&#xea;n con trong c&#xe1;c ho&#x1ea1;t &#x111;&#x1ed9;ng">
+<edge COLOR="#990000" WIDTH="1"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1513606501375" ID="ID_1830513503" MODIFIED="1513607341400" STYLE="fork" TEXT="&#x110;&#x1ed9;ng vi&#xea;n, khen ng&#x1ee3;i con">
+<edge COLOR="#990000" WIDTH="1"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1513606527638" ID="ID_1802120185" MODIFIED="1513607344956" STYLE="fork" TEXT="T&#xed;ch c&#x1ef1;c &#x111;&#x1eb7;t nh&#x1eef;ng c&#xe2;u h&#x1ecf;i &quot;c&#xe1;i g&#xec;&quot;">
+<edge COLOR="#990000" WIDTH="1"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1513606552359" ID="ID_1308121304" MODIFIED="1513607347892" STYLE="fork" TEXT="&#x110;&#x1ec3; cho con &#x111;&#x1b0;&#x1ee3;c t&#x1ef1; tay l&#xe0;m">
+<edge COLOR="#990000" WIDTH="1"/>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1513606846730" ID="ID_381436872" MODIFIED="1513607350227" STYLE="fork" TEXT="G&#x1ee3;i &#xfd; con quan s&#xe1;t v&#xe0; nh&#x1edb; l&#x1ea1;i tr&#xf2; &#x111;&#xe3; ch&#x1a1;i khi ra &#x111;&#x1b0;&#x1edd;ng">
+<edge COLOR="#990000" WIDTH="1"/>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+</node>
+</map>
